ec9a78dc56fa3c889bbfb35e21d96873e3b02dba
Your Name <you@example.com>
2017-10-11 16:58:03 -0400

    better than u

--- board.cpp
+++ board.cpp
@@ -8,20 +8,21 @@ Board::Board(int size)
 {
 	this->size = size;
 
-	Organism*** theBoard = new Organism**;
+	Organism*** theBoard = new Organism**[size];
 
 	for (int i=0;i<size;i++)
 	{
 		printf("%i\n", i);
-		theBoard[i] = new Organism*;
+		theBoard[i] = new Organism*[size];
 	}
 
 	for(int i=0; i<size; i++){
+		printf("\n");
 		for(int j=0; j<size; j++){
-			printf("%i    %i\n", i,j);
-			Organism* o = 0;
+			printf("   %p  ",theBoard[i][j]);
+			//Organism* o = 0;
 
-			theBoard[i][j] = o;
+			//theBoard[i][j] = o;
 		}
 	}
 	printf("test\n");
